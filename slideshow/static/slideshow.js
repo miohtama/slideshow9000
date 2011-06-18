123a24883111f809efa6927aa2102371c4f7825a
Mikko <mikko@mfabrik.com>
2011-06-18 22:06:51 +0300

    Basic animation loop d0n3

--- slideshow/static/slideshow.js
+++ slideshow/static/slideshow.js
@@ -5,20 +5,45 @@ slideshow = {
 		this.createImages();
 		
 		this.clock = 0;
+		this.width = this.height = 400;
 		
-		this.loop();
+		
 	},	
 		
 	createCanvas : function() {
         this.canvas = document.getElementById("slideshow");
-        this.ctx = canvas.getContext("2d");
+        this.ctx = this.canvas.getContext("2d");
         
 	},
 	
 	createImages : function() {
-		this.images = [];	
-		this.images.push(new Image("/images/asikainen.png"));
-		this.images.push(new Image("/images/kakku.png"));		
+   
+        var targets = [
+		  "/static/images/asikainen.png",
+		  "/static/images/kakku.png"
+		];
+			
+		var readyCount = 0;
+        var images = this.images = [];   
+
+        var self = this;
+
+        function ready() {
+			console.log("Ready!");
+			readyCount++;
+			if(readyCount >= targets.length) {
+				self.loop();
+			}
+		}				
+				
+        targets.forEach(function(src) {
+			console.log("Loading img:" + src);
+			var img = new Image();			
+			img.onload = ready;		
+			img.src = src;	
+			images.push(img);
+		});    
+	
 	},
 	
 	
@@ -27,28 +52,36 @@ slideshow = {
 	},
 	
 	loop : function() {
+	   console.log("Entering animation loop");
 	   this.prepareTick();	
 	},
 	
 	tick : function() {
+		
 	  var delta = 0.1;
 	  this.clock += 0.1;	  
-	  animate(delta, time); 
+	  this.animate(delta, this.clock); 
+	  this.prepareTick();
 	},
 	
 	animate : function(delta, time) {
+
+        var ctx = this.ctx;
+
 		var x = time * 5;
 
-        ctx = this.ctx;
+        ctx.clearRect(0, 0, this.width, this.height); // clear canvas
 		
         ctx.fillStyle = "rgb(200,0,0)";
         ctx.fillRect (x+10, 10, x+55, 50);
         
         ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
         ctx.fillRect (30, 30, 55, 50);      
+		
+		ctx.drawImage(this.images[0], 0, x);
 		 
 	}
 	
 };
 
-$(document).ready(slideshow.init);
+$(document).ready($.proxy(slideshow.init, slideshow));
